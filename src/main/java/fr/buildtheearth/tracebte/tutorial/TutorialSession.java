dfef983b9242a71e9dbbbd8d5545004f3a5d153b
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-05-09 01:51:36 +0000

    Update TutorialSession.java

--- src/main/java/fr/buildtheearth/tracebte/tutorial/TutorialSession.java
+++ src/main/java/fr/buildtheearth/tracebte/tutorial/TutorialSession.java
@@ -1,5 +1,7 @@
 package fr.buildtheearth.tracebte.tutorial;
 
+import org.bukkit.Location;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -9,36 +11,27 @@ public final class TutorialSession {
     private final UUID playerId;
     private TutorialStep step;
     private int tpllCount;
+    private int lineCount;
     private final Set<Integer> validatedCorners;
-
-    private boolean pendingTpll = false;
-    private double pendingLat;
-    private double pendingLon;
+    private final Set<Location> placedBlocks;
 
     public TutorialSession(UUID playerId) {
         this.playerId = playerId;
         this.step = TutorialStep.INTRO;
         this.tpllCount = 0;
+        this.lineCount = 0;
         this.validatedCorners = new HashSet<>();
+        this.placedBlocks = new HashSet<>();
     }
 
-    public UUID getPlayerId() { return playerId; }
-
-    public TutorialStep getStep() { return step; }
-    public void setStep(TutorialStep step) { this.step = step; }
-
-    public int getTpllCount() { return tpllCount; }
-    public void incrementTpll() { tpllCount++; }
-
-    public Set<Integer> getValidatedCorners() { return validatedCorners; }
-    public boolean allCornersPlaced() { return validatedCorners.size() >= 4; }
-
-    public void setPendingTpll(double lat, double lon) {
-        this.pendingTpll = true;
-        this.pendingLat = lat;
-        this.pendingLon = lon;
-    }
-
-    public boolean hasPendingTpll() { return pendingTpll; }
-    public void clearPendingTpll() { this.pendingTpll = false; }
+    public UUID getPlayerId()                  { return playerId; }
+    public TutorialStep getStep()              { return step; }
+    public void setStep(TutorialStep step)     { this.step = step; }
+    public int getTpllCount()                  { return tpllCount; }
+    public void incrementTpll()                { tpllCount++; }
+    public int getLineCount()                  { return lineCount; }
+    public void incrementLine()                { lineCount++; }
+    public Set<Integer> getValidatedCorners()  { return validatedCorners; }
+    public Set<Location> getPlacedBlocks()     { return placedBlocks; }
+    public boolean allCornersPlaced()          { return validatedCorners.size() >= 4; }
 }
